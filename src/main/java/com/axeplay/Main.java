d8198cebd6bc0b930db74d19c13e87ea8bf47a4d
Артём <89867564946@mail.ru>
2024-07-20 16:29:52 +0300

    First commit with ssh

--- src/main/java/com/axeplay/Main.java
+++ src/main/java/com/axeplay/Main.java
@@ -2,6 +2,6 @@ package com.axeplay;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("First git push width SSH!");
     }
 }
